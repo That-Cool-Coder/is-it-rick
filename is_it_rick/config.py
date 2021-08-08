17da29058f89cb227a9a1495e337e29e44d94c02
ThatCoolCoder <That-Cool-Coder@github.com>
2021-08-08 12:23:19 +0930

    delete unneeded newline

--- is_it_rick/config.py
+++ is_it_rick/config.py
@@ -18,6 +18,5 @@ SESSION_ID_DATABASE_FILE = os.path.join(DATABASE_DIRECTORY, 'session_ids.json')
 VERSION = '1.1.0-beta'
 DATABASE_READ_INTERVAL = 10
 SESSION_ID_DURATION = 60 * 60 # 1 hour (in seconds)
-
 SESSION_ID_COOKIE_NAME = 'isItRickSessionId'
 RICK_ROLLS_DISPLAYED = 10 # how many rick rolls are displayed on the management page?
